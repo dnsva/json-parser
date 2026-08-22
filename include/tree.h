434a5b34b9941ce315baabab818202708d33bba5
dnsva <anna.dnsva@gmail.com>
2026-08-21 23:49:58 -0400

    planned ast

--- include/tree.h
+++ include/tree.h
@@ -1,15 +1,45 @@
-//nodes
 /*
 
-parent child siblings 
+planning/notes:
 
-Token t; //current token root
+note to self: dont think of it as OOP. this isnt a tree like that
 
-Token ls, rs; //right side and leftside
+you just have one VALUE. and VALUE can be object, array, number, string, boolean, null type
 
-bool end?
+store in variant 
 
+return:
+value of variant is string or array or object or whatever 
+- could also output the json contents in the data structures
 
-note: (from wikipedia) grouping parentheses are implicit in the tree structure
+basically
+json (a bunch of chars) -> data structures
+
+VALUE (options)
+array: [VALUE, VALUE, VALUE]
+object: {"string": VALUE, "string": VALUE}
+or just VALUE
+
+how to store:
+
+object - unordered_map<string, value>
+array - vector of VALUES (this is legal)
+number - double (evaluate the Es)
+string - string
+true/false - bool 
+null - null_type (make an empty struct for this just to have the keyword)
 
 */
+
+#include <iostream>
+#include <variant>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+//define a type for null:
+
+struct NULL_TYPE{};
+
+std::variant<unordered_map<string, VALUE>,vector<VALUE>, double, string, bool, NULL_TYPE> VALUE; 
